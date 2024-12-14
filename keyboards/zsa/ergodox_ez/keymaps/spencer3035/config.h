b98a8393efaef73738067d1e62b02dd80b3969ee
Spencer <spencer3035@gmail.com>
2024-12-14 11:44:43 -0700

    Config generated from oryx

---
+++ keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
@@ -0,0 +1,9 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define SERIAL_NUMBER "v9axa/OjL9y"
+#define LAYER_STATE_8BIT
+
