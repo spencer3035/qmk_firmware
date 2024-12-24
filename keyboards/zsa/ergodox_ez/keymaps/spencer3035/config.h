3a0cabd0ad8f1230ca3a1b27aeca382650b01693
Spencer <spencer3035@gmail.com>
2024-12-24 09:56:47 -0700

    Added alt to gaming layer and switched symbol layer switch location

--- keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
+++ keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
@@ -4,7 +4,7 @@
 */
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "v9axa/nQpqD"
+#define SERIAL_NUMBER "v9axa/oxEPd"
 #define LAYER_STATE_8BIT
 //#define HOLD_ON_OTHER_KEY_PRESS
 #define PERMISSIVE_HOLD
