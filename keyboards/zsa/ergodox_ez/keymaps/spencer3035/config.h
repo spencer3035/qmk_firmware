d06ebe0d0c684fb6b5165addc3ddea1aa7689cb7
Spencer <spencer3035@gmail.com>
2025-02-02 22:58:01 -0700

    Updated oryx hash

--- keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
+++ keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
@@ -7,6 +7,6 @@
 #define TAPPING_TERM 170
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "v9axa/No5qod"
+#define SERIAL_NUMBER "v9axa/Dz46MM"
 #define LAYER_STATE_16BIT
 #define PERMISSIVE_HOLD
