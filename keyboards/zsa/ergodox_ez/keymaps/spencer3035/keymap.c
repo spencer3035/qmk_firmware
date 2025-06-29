fcf88ecfd602034df982b0ee5b9569b29d329497
Spencer <spencer3035@gmail.com>
2025-06-29 11:19:38 -0600

    Switched around QK_REP and moved escape to thumb

--- keyboards/zsa/ergodox_ez/keymaps/spencer3035/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/spencer3035/keymap.c
@@ -46,11 +46,11 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE_LAYER] = LAYOUT_ergodox(
 ____,      KC_1,     KC_2,     KC_3,      KC_4,      KC_5,           ____,
-____,      KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,           KC_ESCAPE,
+____,      KC_Q,     KC_W,     KC_E,      KC_R,      KC_T,           ____,
 KC_LGUI,   KC_A,     KC_S,     KC_D,      KC_F,      KC_G,           /*_*/
 ____,      KC_Z,     KC_X,     KC_C,      KC_V,      KC_B,           CW_TOGG,
-____,      ____,     ____,     ____,      QK_REP,    /*_*/           /*_*/
-/*_*/      /*_*/     /*_*/     /*_*/      /*_*/      OSM(MOD_LGUI),  DM_PLY1,
+____,      ____,     ____,     ____,      KC_ESC,    /*_*/           /*_*/
+/*_*/      /*_*/     /*_*/     /*_*/      /*_*/      OSM(MOD_LGUI),  QK_REP,
 /*_*/      /*_*/     /*_*/     /*_*/      /*_*/      /*_*/           DM_RSTP,
 /*_*/      /*_*/     /*_*/     /*_*/      KC_SPACE,  OSM(MOD_LSFT),  KC_LCTL,
 TO(1),     KC_6,     KC_7,     KC_8,      KC_9,      KC_0,           KC_DOT,
@@ -58,7 +58,7 @@ TO(6),     KC_Y,     KC_U,     KC_I,      KC_O,      KC_P,           KC_BSLS,
 /*_*/      KC_H,     KC_J,     KC_K,      KC_L,      KC_SCLN,        KC_QUOTE,
 KC_MINUS,  KC_N,     KC_M,     KC_COMMA,  KC_DOT,    KC_SLASH,       KC_COLN,
 /*_*/      /*_*/     OSL(2),   KC_LEFT,   KC_DOWN,   KC_UP,          KC_RIGHT,
-DM_PLY2,   KC_TAB,   /*_*/     /*_*/      /*_*/      /*_*/           /*_*/
+QK_AREP,   KC_TAB,   /*_*/     /*_*/      /*_*/      /*_*/           /*_*/
 OSL(5),    /*_*/     /*_*/     /*_*/      /*_*/      /*_*/           /*_*/
 KC_LALT,   KC_BSPC,  KC_ENTER  /*_*/      /*_*/      /*_*/           /*_*/
   ),
