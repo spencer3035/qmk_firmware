c1130213839da0e7c11e1c7e0bdb2a343a3292b9
Spencer <spencer3035@gmail.com>
2025-08-15 10:32:04 -0600

    Updated serial number for keymapp

--- keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
+++ keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
@@ -7,6 +7,6 @@
 #define TAPPING_TERM 170
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "v9axa/7vQxMy"
+#define SERIAL_NUMBER "v9axa/GGw0da"
 #define LAYER_STATE_8BIT
 #define PERMISSIVE_HOLD
