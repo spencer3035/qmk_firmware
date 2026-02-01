db0c11fb5b44b3fd93699200aa22fb78b94a643d
Spencer <spencer3035@gmail.com>
2026-02-01 11:44:53 -0700

    Updated serial code

--- keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
+++ keyboards/zsa/ergodox_ez/keymaps/spencer3035/config.h
@@ -7,6 +7,6 @@
 #define TAPPING_TERM 170
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "v9axa/9DRLaK"
+#define SERIAL_NUMBER "jmMna/x9dnXo"
 #define LAYER_STATE_8BIT
 #define PERMISSIVE_HOLD
