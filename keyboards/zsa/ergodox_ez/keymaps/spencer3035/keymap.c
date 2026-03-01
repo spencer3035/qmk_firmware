4dbea10b4c49716fbadf81233a86a6a043eb1385
Spencer <spencer3035@gmail.com>
2026-03-01 04:31:52 -0700

    Added home row mods to main layer

--- keyboards/zsa/ergodox_ez/keymaps/spencer3035/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/spencer3035/keymap.c
@@ -41,22 +41,22 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE_LAYER] = LAYOUT_ergodox(
-KC_GRAVE,  KC_1,     KC_2,      KC_3,      KC_4,      KC_5,           ____,
-KC_TAB,    KC_Q,     KC_W,      KC_E,      KC_R,      KC_T,           ____,
-KC_LGUI,   KC_A,     KC_S,      KC_D,      KC_F,      KC_G,           /*_*/
-KC_LSFT,   KC_Z,     KC_X,      KC_C,      KC_V,      KC_B,           CW_TOGG,
-KC_LCTL,   KC_LALT,  QK_LOCK,   ____,      KC_ESC,    /*_*/           /*_*/
-/*_*/      /*_*/     /*_*/      /*_*/      /*_*/      OSM(MOD_LGUI),  QK_REP,
-/*_*/      /*_*/     /*_*/      /*_*/      /*_*/      /*_*/           ____,
-/*_*/      /*_*/     /*_*/      /*_*/      KC_SPACE,  OSM(MOD_LSFT),  KC_LCTL,
-TO(ALT),   KC_6,     KC_7,      KC_8,      KC_9,      KC_0,           KC_DOT,
-TO(STL),   KC_Y,     KC_U,      KC_I,      KC_O,      KC_P,           KC_BSLS,
-/*_*/      KC_H,     KC_J,      KC_K,      KC_L,      KC_SCLN,        KC_QUOTE,
-KC_MINUS,  KC_N,     KC_M,      KC_COMMA,  KC_DOT,    KC_SLASH,       KC_COLN,
-/*_*/      /*_*/     OSL(SYL),  KC_LEFT,   KC_DOWN,   KC_UP,          KC_RIGHT,
-QK_REP,    KC_TAB,   /*_*/      /*_*/      /*_*/      /*_*/           /*_*/
-____,      /*_*/     /*_*/      /*_*/      /*_*/      /*_*/           /*_*/
-KC_LALT,   KC_BSPC,  KC_ENTER   /*_*/      /*_*/      /*_*/           /*_*/
+KC_GRAVE,  KC_1,     KC_2,               KC_3,               KC_4,               KC_5,           ____,
+KC_TAB,    KC_Q,     KC_W,               KC_E,               KC_R,               KC_T,           ____,
+KC_LGUI,   KC_A,     MT(MOD_LALT,KC_S),  MT(MOD_LGUI,KC_D),  MT(MOD_LCTL,KC_F),  KC_G,           /*_*/
+KC_LSFT,   KC_Z,     KC_X,               KC_C,               KC_V,               KC_B,           CW_TOGG,
+KC_LCTL,   KC_LALT,  QK_LOCK,            ____,               KC_ESC,             /*_*/           /*_*/
+/*_*/      /*_*/     /*_*/               /*_*/               /*_*/               ____,           QK_REP,
+/*_*/      /*_*/     /*_*/               /*_*/               /*_*/               /*_*/           ____,
+/*_*/      /*_*/     /*_*/               /*_*/               KC_SPACE,           OSM(MOD_LSFT),  ____,
+TO(ALT),   KC_6,     KC_7,               KC_8,               KC_9,               KC_0,           KC_DOT,
+TO(STL),   KC_Y,     KC_U,               KC_I,               KC_O,               KC_P,           KC_BSLS,
+/*_*/      KC_H,     MT(MOD_RCTL,KC_J),  MT(MOD_RGUI,KC_K),  MT(MOD_RALT,KC_L),  KC_SCLN,        KC_QUOTE,
+KC_MINUS,  KC_N,     KC_M,               KC_COMMA,           KC_DOT,             KC_SLASH,       KC_COLN,
+/*_*/      /*_*/     OSL(SYL),           KC_LEFT,            KC_DOWN,            KC_UP,          KC_RIGHT,
+QK_REP,    KC_TAB,   /*_*/               /*_*/               /*_*/               /*_*/           /*_*/
+____,      /*_*/     /*_*/               /*_*/               /*_*/               /*_*/           /*_*/
+____,      KC_BSPC,  KC_ENTER            /*_*/               /*_*/               /*_*/           /*_*/
   ),
   [SYMBOLS_LAYER] = LAYOUT_ergodox(
 ____,  KC_KP_1,     KC_KP_2,      KC_KP_3,      KC_KP_4,    KC_KP_5,      KC_NUM_LOCK,
@@ -106,7 +106,7 @@ XXXX,      XXXX,      QK_LOCK,            XXXX,               KC_ESC,           
 TO(BAL),   KC_6,      KC_7,               KC_8,               KC_9,               KC_0,           KC_DOT,
 TO(BAL),   KC_MINUS,  KC_F,               KC_O,               KC_U,               KC_J,           KC_BSLS,
 /*_*/      KC_Y,      MT(MOD_RCTL,KC_H),  MT(MOD_RGUI,KC_A),  MT(MOD_RALT,KC_E),  KC_I,           KC_SCLN,
-XXXX,      KC_K,      KC_P,               KC_COMMA,           KC_DOT,             KC_SLASH,       KC_COLN,
+XXXX,      KC_K,      KC_P,               KC_COMMA,           KC_DOT,             KC_SLASH,       KC_QUOTE,
 /*_*/      /*_*/      OSL(SYL),           KC_LEFT,            KC_DOWN,            KC_UP,          KC_RIGHT,
 QK_REP,    KC_TAB,    /*_*/               /*_*/               /*_*/               /*_*/           /*_*/
 XXXX,      /*_*/      /*_*/               /*_*/               /*_*/               /*_*/           /*_*/
